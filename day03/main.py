175f85443f2d7c9fa05eeace978777e9b6d9dc54
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2022-12-03 12:09:23 +0000

    ref: Simplify day 3.

--- day03/main.py
+++ day03/main.py
@@ -7,17 +7,16 @@ def split(line):
 
 
 def common_item(iterable):
-    for first in reduce(lambda acc, x: acc & x, map(set, iterable)):
-        return first
+    return reduce(lambda acc, x: acc & x, map(set, iterable)).pop()
 
 
-def get_ord(ch):
+def priority(ch):
     return ord(ch) - 38 if ch < "a" else ord(ch) - 96
 
 
 with open("day03/input.txt", mode="r", encoding="utf-8") as file:
-    lines = [line[:-1] for line in file.readlines()]
+    lines = [line.rstrip() for line in file]
     items = [common_item(split(line)) for line in lines]
     groups = [common_item(lines[i : i + 3]) for i in range(0, len(lines), 3)]
-    print("1:", sum(map(get_ord, items)))
-    print("2:", sum(map(get_ord, groups)))
+    print("1:", sum(map(priority, items)))
+    print("2:", sum(map(priority, groups)))
